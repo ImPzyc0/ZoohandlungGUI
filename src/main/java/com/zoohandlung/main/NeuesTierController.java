2242b8b97faf8bfe90327a9b4e171444021c4c0b
ImPzyc0 <dani00cooli@gmail.com>
2023-08-29 22:41:29 +0200

    Neues Tier fenster angefangen

--- src/main/java/com/zoohandlung/main/NeuesTierController.java
+++ src/main/java/com/zoohandlung/main/NeuesTierController.java
@@ -4,7 +4,11 @@ import com.zoohandlung.Tier;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +17,18 @@ public class NeuesTierController implements Initializable {
 
     @FXML
     private ChoiceBox<String> tiere = new ChoiceBox<>();
+
+    @FXML
+    private StackPane hundPane, katzePane, pferdPane;
+
+    @FXML
+    private TextField katzeAlter, hundAlter, pferdAlter, katzePreis, hundPreis, pferdPreis, katzeName, hundName, pferdName;
+
+    @FXML
+    private ChoiceBox<String> katzeRasse, hundRasse, pferdRasse;
+
+    @FXML
+    private Button katzeEnter, hundEnter, pferdEnter;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -23,7 +39,6 @@ public class NeuesTierController implements Initializable {
     protected void getTier(ActionEvent event){
 
         String tier = tiere.getValue();
-
         switch (tier){
             case "Pferd":
                 zeigePferdView();
@@ -40,13 +55,19 @@ public class NeuesTierController implements Initializable {
     }
 
     protected void zeigeHundView(){
-        System.out.println("ö2");
+        hundPane.setVisible(false);
+        pferdPane.setVisible(true);
+        katzePane.setVisible(true);
     }
 
     protected void zeigeKatzeView(){
-
+        hundPane.setVisible(true);
+        pferdPane.setVisible(true);
+        katzePane.setVisible(false);
     }
     protected void zeigePferdView(){
-
+        hundPane.setVisible(true);
+        pferdPane.setVisible(false);
+        katzePane.setVisible(true);
     }
 }
